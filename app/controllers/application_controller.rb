75bd92fe79889ba9cf99876dd98b0298e775e5ad
pirateontheseas <ian370zzx+p@gmail.com>
2019-11-12 23:58:21 +0000

    nice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,46 @@
 class ApplicationController < ActionController::Base
+  def cuisinehuh
+    biglist = Cuisine.all
+    render({ :plain =>  biglist.to_json })
+  end
+
+  def disheshuh
+    biglist = Dish.all
+    render({ :plain =>  biglist.to_json })
+  end  
+
+  def disheswot
+    title = params.fetch(:dishid)
+    piratebooty = Dish.where({ :id => title }).at(0)
+    render({ :plain =>  piratebooty.to_json })
+  end  
+
+  def neighborwhere
+    biglist = Neighborhood.all
+    render({ :plain =>  biglist.to_json })
+  end
+
+  def venuewot
+    biglist = Venue.all
+    render({ :plain =>  biglist.to_json })
+  end
+
+  def userwot
+    biglist = User.all
+    render({ :plain =>  biglist.to_json })
+  end
+
+  def venuehuh
+    title = params.fetch(:venueid)
+    piratebooty = Venue.where({ :id => title }).at(0)
+    render({ :plain =>  piratebooty.to_json })
+  end
+
+  def userhuh
+    title = params.fetch(:userid)
+    piratebooty = User.where({ :id => title }).at(0)
+    render({ :plain =>  piratebooty.to_json })
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
 
+  match("/cuisines", { :controller => "application", :action => "cuisinehuh", :via => "get" })
 
+  match("/dishes", { :controller => "application", :action => "disheshuh", :via => "get" })
+  
+  match("/dishes/:dishid", { :controller => "application", :action => "disheswot", :via => "get" })
+  
+  match("/neighborhoods", { :controller => "application", :action => "neighborwhere", :via => "get" })
+  
+  match("/venues", { :controller => "application", :action => "venuewot", :via => "get" })
 
+  match("/users", { :controller => "application", :action => "userwot", :via => "get" })
+
+  match("/venues/:venueid", { :controller => "application", :action => "venuehuh", :via => "get" })
+  
+  match("/users/:userid", { :controller => "application", :action => "userhuh", :via => "get" })
+
+  
+
+  
   # ====================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
